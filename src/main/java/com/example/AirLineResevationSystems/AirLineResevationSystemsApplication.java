728e38e4bbbde3fb779813faffd2054cf097968a
Ghulam jilani <ghulamjilani577@gmail.com>
2023-02-26 12:24:09 +0500

    Adding files

---
+++ src/main/java/com/example/AirLineResevationSystems/AirLineResevationSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.AirLineResevationSystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirLineResevationSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirLineResevationSystemsApplication.class, args);
+	}
+
+}
