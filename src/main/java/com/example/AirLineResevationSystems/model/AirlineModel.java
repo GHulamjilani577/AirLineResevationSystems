6276c426ac0a253c027ecc945d0c8f1a30fff770
Ghulam jilani <ghulamjilani577@gmail.com>
2023-04-02 13:55:16 +0500

    project done

--- src/main/java/com/example/AirLineResevationSystems/model/AirlineModel.java
+++ src/main/java/com/example/AirLineResevationSystems/model/AirlineModel.java
@@ -79,7 +79,32 @@ public class AirlineModel {
 
         return airlineModel;
     }
+{
+"reservationNumber":"dhkjhf",
+"location":"sdfbgdss",
+"reservationState":"sfds",
+"reservationCreateDate":"sgsdfg",
+"reservationPayment":"sdfbgs",
+"reservationPaymentMode":"sdfgs",
+"reservationBillingAddress":"sghrth",
+        "flightScheduleModel":{
+     "flightScheduleId":"2",
+    "flightCode":"abc",
+    "destination":"cde",
+    "flightStatus":"efg",
+    "flightType":"ddfrr",
+    "departure":"rfrgrf",
+     "time":"dfgtfdh",
+     "airLineFlightModel":{
+         "airLineFlightId":"1",
+      "airlineName":"PIA",
+"type":"Domistic",
+"businessCapacity":"44",
+"economyCapacity":"112",
+"originAirport":"Lahore",
+"destinationAirport":"Islamabad"
 
+     }}}
 
 
 
